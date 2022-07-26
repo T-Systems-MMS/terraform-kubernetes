ad65ed22dcdc41fbd621374f691893d50fc3cab1
mima <Michaela.Mattes@t-systems.com>
2022-07-26 11:05:45 +0200

    add more resource definitions

--- main.tf
+++ main.tf
@@ -9,9 +9,13 @@ resource "kubernetes_service_account" "service_account" {
   for_each = var.kubernetes_service_account
 
   metadata {
-    name      = local.kubernetes_service_account[each.key].metadata.name == "" ? each.key : local.kubernetes_service_account[each.key].metadata.name
-    namespace = local.kubernetes_service_account[each.key].metadata.namespace
+    name          = local.kubernetes_service_account[each.key].metadata.name == "" ? each.key : local.kubernetes_service_account[each.key].metadata.name
+    namespace     = local.kubernetes_service_account[each.key].metadata.namespace
+    annotations   = local.kubernetes_service_account[each.key].metadata.annotations
+    generate_name = local.kubernetes_service_account[each.key].metadata.generate_name
+    labels        = local.kubernetes_service_account[each.key].metadata.labels
   }
+  automount_service_account_token = local.kubernetes_service_account[each.key].automount_service_account_token
 }
 
 resource "kubernetes_cluster_role_binding" "cluster_role_binding" {
@@ -37,9 +41,10 @@ resource "kubernetes_namespace" "namespace" {
   for_each = var.kubernetes_namespace
 
   metadata {
-    name        = local.kubernetes_namespace[each.key].metadata.name == "" ? each.key : local.kubernetes_namespace[each.key].metadata.name
-    annotations = local.kubernetes_namespace[each.key].metadata.annotations
-    labels      = local.kubernetes_namespace[each.key].metadata.labels
+    name          = local.kubernetes_namespace[each.key].metadata.name == "" ? each.key : local.kubernetes_namespace[each.key].metadata.name
+    annotations   = local.kubernetes_namespace[each.key].metadata.annotations
+    generate_name = local.kubernetes_namespace[each.key].metadata.generate_name
+    labels        = local.kubernetes_namespace[each.key].metadata.labels
   }
 }
 
@@ -47,10 +52,30 @@ resource "kubernetes_secret" "secret" {
   for_each = var.kubernetes_secret
 
   metadata {
-    name      = local.kubernetes_secret[each.key].metadata.name == "" ? each.key : local.kubernetes_secret[each.key].metadata.name
-    namespace = local.kubernetes_secret[each.key].metadata.namespace
+    name          = local.kubernetes_secret[each.key].metadata.name == "" ? each.key : local.kubernetes_secret[each.key].metadata.name
+    namespace     = local.kubernetes_secret[each.key].metadata.namespace
+    annotations   = local.kubernetes_secret[each.key].metadata.annotations
+    generate_name = local.kubernetes_secret[each.key].metadata.generate_name
+    labels        = local.kubernetes_secret[each.key].metadata.labels
   }
 
-  data = local.kubernetes_secret[each.key].data
-  type = local.kubernetes_secret[each.key].type
+  data        = local.kubernetes_secret[each.key].data
+  binary_data = local.kubernetes_secret[each.key].binary_data
+  type        = local.kubernetes_secret[each.key].type
+  immutable   = local.kubernetes_secret[each.key].immutable
+}
+
+resource "kubernetes_config_map" "config_map" {
+  for_each = var.kubernetes_config_map
+
+  metadata {
+    name          = local.kubernetes_config_map[each.key].metadata.name == "" ? each.key : local.kubernetes_config_map[each.key].metadata.name
+    namespace     = local.kubernetes_config_map[each.key].metadata.namespace
+    annotations   = local.kubernetes_config_map[each.key].metadata.annotations
+    generate_name = local.kubernetes_config_map[each.key].metadata.generate_name
+    labels        = local.kubernetes_config_map[each.key].metadata.labels
+  }
+
+  data        = local.kubernetes_config_map[each.key].data
+  binary_data = local.kubernetes_config_map[each.key].binary_data
 }
